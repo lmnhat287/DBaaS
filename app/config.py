4f8f6ec03536385cb05fb027e8f3f2a8b0c3c463
lmnhat287 <lmnhat148@gmail.com>
2025-04-28 03:50:03 +0700

    Merge remote changes and resolve conflicts

--- app/config.py
+++ app/config.py
@@ -5,9 +5,13 @@ import os
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "Nhat@20872400")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     ENV = os.environ.get("FLASK_ENV", "development")
+    DEBUG = ENV == "development"
     # MongoDB Atlas URI (cập nhật theo URI thật của bạn)
     MONGO_URI = os.environ.get("MONGO_URI")
     # MongoDB database name
     AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
+    APP_NAME = "DBaaS"
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static", "uploads")
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file size
